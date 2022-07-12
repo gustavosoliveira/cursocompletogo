e9304dd1dc4f7f8645769c68f9cf13ad94f07be2
Gustavo Oliveira <gustavo.santosoliveira@outlook.com.br>
2022-07-12 14:44:54 -0300

    Confirmação

---
+++ pkg1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"https://github.com/gustavosoliveira/cursocompletogo.git"
+)
+
+func main() {
+
+	fmt.Println("Essa é a função main.")
+	Outro()
+	pkg2.Pkg2()
+
+}
